26f17a9f02cf4d8537c1a2ba728f4c80f16548c8
sajid-ali548 <sajidalimugheri548@gmail.com>
2024-10-31 17:45:28 +0500

    test project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      
+    </>
+  )
+}
+
+export default App
